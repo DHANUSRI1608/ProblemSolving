5d871b569a641490dd4a1450077257ffd67b2035
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-05-13 17:16:38 +0530

    2D Manipulation

---
+++ Arrays/Solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solut {
+    public int[] separateDigits(int[] nums) { 
+        List<Integer> arr = new ArrayList<>();
+        for (int i=0;i<nums.length;i++) {
+            String s = String.valueOf(nums[i]);
+            for (int j=0;j<s.length();j++) {
+                arr.add(s.charAt(j)-'0');
+            }
+        }
+        int[] fin = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            fin[i] = arr.get(i);
+        }
+        return fin;
+    }
+} 
