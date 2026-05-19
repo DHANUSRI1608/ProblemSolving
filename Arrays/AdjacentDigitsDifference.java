2b03413249019cfa6ce6db7af6c2b7fcd06c96a0
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-05-19 11:16:58 +0530

    No Concatenation

---
+++ Arrays/AdjacentDigitsDifference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        if(s.length()==1)return false;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(Math.abs((s.charAt(i)-'0')-(s.charAt(i+1)-'0'))>2)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
