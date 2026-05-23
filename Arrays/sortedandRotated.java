bc4122e8b4ad40979b74580059723f03eaba2682
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-05-23 10:28:26 +0530

    O(n) - sortedAndRotated

--- Arrays/sortedandRotated.java
+++ Arrays/sortedandRotated.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Solution {
-    public boolean check(int[] nums) {
+    public boolean sortedandRotated(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > nums[(i + 1) % nums.length]) {
