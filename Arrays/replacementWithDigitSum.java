dc0b6055e031a8df274459dfe15a40a2cf08dfc3
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-05-29 10:34:04 +0530

    Crunching

---
+++ Arrays/replacementWithDigitSum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public static int sum(int n)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            sum+=r;
+            n/=10;
+        }
+        return sum;
+    }
+    public int minElement(int[] nums) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            nums[i]=sum(nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+    return min;
+    }
+}
