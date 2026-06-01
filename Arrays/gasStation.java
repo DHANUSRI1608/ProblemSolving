bed06fe008e0a01d5ed8ae0b1d5214e720fccdc4
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-06-01 20:20:34 +0530

    Fix

--- Arrays/gasStation.java
+++ Arrays/gasStation.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int sGas = 0, sCost = 0, res = 0, total = 0;
+        int sGas = 0, sCost = 0, res = 0, t = 0;
         for (int i = 0; i < gas.length; i++) {
             sGas += gas[i];
             sCost += cost[i];
         }
         if (sGas < sCost) return -1;
         for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
-            if (total < 0) {
-                total = 0;
+            t += gas[i] - cost[i];
+            if (t < 0) {
+                t = 0;
                 res = i + 1;
             }
         }
