9470cccdfe376942bc48375751653f248ad52ac8
DHANUSRI1608 <dhanusriii.1608@gmail.com>
2026-06-06 11:15:32 +0530

    Question auto-immune disease

--- Arrays/autoimmune_disease.java
+++ Arrays/autoimmune_disease.java
@@ -1,11 +1,18 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+//A new medicine named "Medio-cine" is out in the market which helps in 
+// treating auto-immune diseases in humans. These are very common in 0-10 year old 
+// kids or greater than or equal to 81 year old senior citizens. Both these 
+// age groups are considered at a very high risk.
 
+
+//The government wants to distribute the medicine as soon as possible among 
+// all age groups. There is only one(1) political center from where 
+// this medicine is distributed. In a single day we cannot provide to both 
+// high risk and non high risk age groups.Each person requires only one capsule 
+// and there can be L capsules distributed in a day. The high risk humans are supposed 
+// to be considered first.There are 'N' humans and 'L' capsules per day.
+//  Find the minimum number of days required to medicate all the N humans.
 public class autoimmune_disease{
-
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
 
